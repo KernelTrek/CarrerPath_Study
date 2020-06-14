2c15e4021fb0b3a20307cc728c48f360ed890168
Kerneltrek <kerneltrek@gmail.com>
2020-06-14 22:03:12 +0900

    Section 02. Commit
    기초 활용

---
+++ 01_pocuprograming/03_PrimitiveTypesToBinary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03_PrimitiveTypesToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte byteMax = byte.MaxValue;
+            byte byteMin = byte.MinValue;
+
+            int intMax = int.MaxValue;
+            int intMin = int.MinValue;
+
+            long longMax = long.MaxValue;
+            long longMin = long.MinValue;
+
+            char charMax = char.MaxValue;
+            char charMin = char.MinValue;
+
+            Console.WriteLine($"{byteMin} -> {Convert.ToString(byteMin, 2)}");
+            Console.WriteLine($"{byteMax} -> {Convert.ToString(byteMax, 2)}");
+
+            Console.WriteLine($"{intMin} -> {Convert.ToString(intMin, 2)}");
+            Console.WriteLine($"{intMax} -> {Convert.ToString(intMax, 2)}");
+
+            Console.WriteLine($"{longMin} -> {Convert.ToString(longMin, 2)}");
+            Console.WriteLine($"{longMax} -> {Convert.ToString(longMax, 2)}");
+
+            // Char Set - 2 Byte ? 
+            // C# - Char is Unicode (2 Byte)
+            // Other Language - Ascii Code ( 1 Byte)
+            Console.WriteLine($"{charMin} -> {Convert.ToString(charMin, 2)}");
+            Console.WriteLine($"{charMax} -> {Convert.ToString(charMax, 2)}");
+
+
+        }
+    }
+}
---
+++ 01_pocuprograming/04_Variables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code Standard
+            // Local Variable - Camel Case 
+            int int1 = 2;
+            int int2 = 10;
+
+
+            // What Is "f" ?
+            // f는 접미어(postfix) 로 C# 에서는 기본 자료형이 Double 형이다 보니,
+            // float 의 경우 f를 붙여 준다. 
+            float float1 = 10.23f;
+            float float2 = 3.4f;
+
+            double double1 = 11.0;
+            double double2 = 5.234;
+
+            char char1 = 'x';
+            char char2 = '$';
+
+            // Char 는 기본이 Ascii 이다 ... 그런데 혹시... 300 은 어떤 문자일까 ?
+            // 129 부터 ? 가 되는 것으로 봐서, EUC-KR 이나 기타 기능들도 지원하지 않는 것 같다. 
+            char testChar = (char)300;
+
+            Console.WriteLine($"{int1} + {int2} = {int1 + int2}");
+            Console.WriteLine($"{int2} + {int1} = {int2 - int1}");
+            Console.WriteLine($"{float1} + {float2} = {float1 + float2}");
+            Console.WriteLine($"{double1} + {double2} = {double1 + double2}");
+            Console.WriteLine($"{char1} + {char2} = {char1 + char2}");
+            Console.WriteLine($"char set 300 ? {testChar}"); // 그냥 ? 네... 안쓰나.. 
+
+        }
+    }
+}
