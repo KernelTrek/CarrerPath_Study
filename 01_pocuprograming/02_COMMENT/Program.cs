58b603cdb8e731823d1f091c36b3958282eda744
Kerneltrek <kerneltrek@gmail.com>
2020-06-14 21:50:27 +0900

    Initialize COMP1500

---
+++ 01_pocuprograming/02_COMMENT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02_COMMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            /*Console.WriteLine("Hello, World!");
+            */
+
+            //Console.WriteLine("Hello World!");
+
+        }
+    }
+}
